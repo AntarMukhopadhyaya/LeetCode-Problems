ca25973cfe35f7dd81039e76e914848786b55dc4
Antar Mukhopadhaya <37949912+AntarMukhopadhyaya@users.noreply.github.com>
2024-11-29 20:37:32 +0530

    Time: 8 ms (14.55%), Space: 18.8 MB (9.44%) - LeetHub

---
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -0,0 +1,36 @@
+import math
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+       
+        count1 = count2 = 0
+        ele1 = ele2 = -math.inf
+        for i in range(n):
+            if count1 == 0 and nums[i] != ele2:
+                count1 = 1
+                ele1 = nums[i]
+            elif count2 == 0 and nums[i] != ele1:
+                count2 =1
+                ele2 = nums[i]
+            elif nums[i] == ele1:
+                count1 += 1
+            elif nums[i] == ele2:
+                count2 += 1
+            else:
+                count1  -= 1
+                count2 -= 1
+        count1 = count2 = 0
+        for i in range(n):
+            if nums[i] == ele1:
+                count1 += 1
+            if nums[i] == ele2:
+                count2 += 1
+        ans = []
+        mini = int(n/3)+1
+        if count1 >= mini:
+            ans.append(ele1)
+        if count2 >= mini:
+            ans.append(ele2)
+        ans.sort()
+        return ans
+        
