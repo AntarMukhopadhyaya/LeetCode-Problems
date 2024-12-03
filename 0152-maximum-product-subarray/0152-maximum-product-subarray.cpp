27f930423502930bd45391a27462f7111fd546f5
Antar Mukhopadhaya <37949912+AntarMukhopadhyaya@users.noreply.github.com>
2024-12-03 22:40:42 +0530

    Time: 0 ms (100.00%), Space: 17.8 MB (5.02%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
+        // The answer will lie on either suffix or prefix
+        // when suffix == 0 make it 1
+        // when prefix == 0 make it 1
+        // prefix starts from start of the array
+        // suffix starts from end of the array
+        // the answer is the max of suffix and prefix
+        
         int maxi = INT_MIN;
         int n = nums.size();
         int prefix = 1, suffix = 1;
         for(int i =0; i < n; i++){
+            
             if(prefix == 0) prefix = 1;
             if(suffix == 0) suffix = 1;
             prefix = prefix * nums[i];
